7931dffa338cc54db475c64198e3a2f753991279
ForkBombGIT <erosdipede@gmail.com>
2019-10-02 12:03:27 -0400

    Add material.ui, and react-localization

--- tbw-website/src/App.js
+++ tbw-website/src/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import './Styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{this.props.title}</h1>
+        <AppBar>
+          <Toolbar>
+            <Typography>{this.props.title}</Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
