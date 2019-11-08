4fb0414999a922d05823858c4d1b70c25e96dccd
ForkBombGIT <erosdipede@gmail.com>
2019-11-08 16:53:40 -0500

    Add skeleton code to toolbar

--- tbw-website/src/Components/CustomToolbar.js
+++ tbw-website/src/Components/CustomToolbar.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
 
 class CustomToolbar extends Component {
   render() {
     return (
-      <AppBar>
+      <AppBar position="static">
         <Toolbar>
-          <Typography>{this.props.title}</Typography>
+          <Typography class="title">{this.props.title}</Typography>
+          <Button>
+            About
+          </Button>
+          <Button>
+            Services
+          </Button>
+          <Button>
+            Projects
+          </Button>
+          <Button>
+            Careers
+          </Button>
+          <Button>
+            Contact
+          </Button>
         </Toolbar>
       </AppBar>
     );
---
+++ tbw-website/src/Values/colors.js
@@ -0,0 +1,14 @@
+//Primary
+const primary	= 	'#0B1210';
+const secondary	= 	'#A31621';
+const gray	    =  	'#A9A9A9';
+const white     =   '#FFFAFA';
+
+const Colours = {
+	primary: {
+	},
+	sec: {
+	},
+	neutral: {
+	}
+};
