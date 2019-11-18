900140a40a6856e53ac54c1145d871c6a4c049fe
Liannus <josh-gorman@hotmail.com>
2019-11-18 12:47:25 -0500

    TBW-10 Added fontawesome and roboto, organized images and fonts into assets folder

--- tbw-website/src/Pages/LandingPage/LandingPage.jsx
+++ tbw-website/src/Pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { CustomToolbar } from "Components/";
+import { CustomToolbar, CustomFooter } from "Components/";
 
 function LandingPage() {
   const { t } = useTranslation();
-  return <CustomToolbar title={t("title")} />;
+  return (
+    <>
+      <CustomToolbar title={t("title")} />
+      <CustomFooter />
+    </>
+  );
 }
 
 export default LandingPage;
