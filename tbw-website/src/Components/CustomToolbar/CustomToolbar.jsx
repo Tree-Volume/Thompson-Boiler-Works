80a30b906cb8bf6f4fb1dbae96385efda761f43a
ForkBombGIT <erosdipede@gmail.com>
2019-11-20 21:05:03 -0500

    Add logo to toolbar

--- tbw-website/src/Components/CustomToolbar/CustomToolbar.jsx
+++ tbw-website/src/Components/CustomToolbar/CustomToolbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { AppBar, Toolbar, Button, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
 import "./CustomToolbar.scss";
+import logo from "Assets/images/tbw-logo.png"
 
 const CustomToolbar = ({ title, options }) => (
-  <AppBar className="AppBar" position="static">
+  <AppBar className="toolbar" position="static">
     <Toolbar>
-      <Typography className="nav-title">{title}</Typography>
+      <a class="toolbar-logo" href="/">
+        <div class="toolbar-logo-image">
+              <img src={logo} class="img-fluid" alt={title}/>
+        </div>
+      </a>
       {options.map((value,index) => {
         return <Button key={index}>{value}</Button>;
       })}
