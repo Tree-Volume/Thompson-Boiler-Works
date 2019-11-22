d20bb663b01e026798351173376c6e0918b6021f
ForkBombGIT <erosdipede@gmail.com>
2019-11-22 01:21:18 -0500

    [WIP] Add divider

---
+++ tbw-website/src/Components/CustomDivider/CustomDivider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./CustomDivider.scss";
+
+const CustomDivider = () => {
+  return (
+    <div class="divider">
+      <div class="filter-gradient">
+        <div class="filter-black">
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomDivider;
--- tbw-website/src/Pages/LandingPage/LandingPage.jsx
+++ tbw-website/src/Pages/LandingPage/LandingPage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography";
+import CustomDivider from "Components/CustomDivider/CustomDivider";
 
 import "./LandingPage.scss";
 
 function LandingPage() {
   return (
-      <div class="landing">
-        <div class="landing-title" >
+    <div class="landing">
+      <div class="landing-title">
+        <div class="landing-title-text">
           <Typography variant="h1">Thompson Boiler Works</Typography>
           <Typography variant="h2">Welding since 1988</Typography>
         </div>
       </div>
+      <CustomDivider></CustomDivider>
+    </div>
   );
 }
 
