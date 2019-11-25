54ddbb715aa51ad84d9fd0e5cb613f90be2aea93
ForkBombGIT <erosdipede@gmail.com>
2019-11-25 13:36:16 -0500

    Add style changes
    - added skeleton code for landing content

--- tbw-website/src/Pages/LandingPage/LandingPage.jsx
+++ tbw-website/src/Pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import CustomDivider from "Components/CustomDivider/CustomDivider";
 import { Typography, Container, Button } from "@material-ui/core/";
+import landing from "Assets/images/landing.png"
 
 import "./LandingPage.scss";
+
 
 function LandingPage() {
   return (
@@ -38,11 +40,39 @@ function LandingPage() {
           <Typography variant="h2" align="center">
             Services
           </Typography>
+          <Typography className="landing-section-body" variant="h6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor urna quis
+            sapien rutrum volutpat. Nullam lectus magna, lacinia nec orci quis, cursus convallis
+            magna. Cras dictum laoreet tellus a rutrum. Donec ac commodo augue. Donec enim metus,
+            fringilla id suscipit ac, malesuada non ligula. Aenean ultricies quam ut sem eleifend,
+            quis condimentum diam rutrum. Duis pulvinar at turpis vitae dignissim. Proin vehicula
+            dignissim cursus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+            porttitor urna quis sapien rutrum volutpat. Nullam lectus magna, lacinia nec orci quis,
+            cursus convallis magna. Cras dictum laoreet tellus a rutrum. Donec ac commodo augue.
+            Donec enim metus, fringilla id suscipit ac, malesuada non ligula. Aenean ultricies quam
+            ut sem eleifend, quis condimentum diam rutrum. Duis pulvinar at turpis vitae dignissim.
+            Proin vehicula dignissim cursus.
+          </Typography>
+          <Button variant="contained">Learn more about our services!</Button>
         </div>
         <div class="landing-section">
           <Typography variant="h2" align="center">
             Projects
           </Typography>
+          <Typography className="landing-section-body" variant="h6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor urna quis
+            sapien rutrum volutpat. Nullam lectus magna, lacinia nec orci quis, cursus convallis
+            magna. Cras dictum laoreet tellus a rutrum. Donec ac commodo augue. Donec enim metus,
+            fringilla id suscipit ac, malesuada non ligula. Aenean ultricies quam ut sem eleifend,
+            quis condimentum diam rutrum. Duis pulvinar at turpis vitae dignissim. Proin vehicula
+            dignissim cursus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+            porttitor urna quis sapien rutrum volutpat. Nullam lectus magna, lacinia nec orci quis,
+            cursus convallis magna. Cras dictum laoreet tellus a rutrum. Donec ac commodo augue.
+            Donec enim metus, fringilla id suscipit ac, malesuada non ligula. Aenean ultricies quam
+            ut sem eleifend, quis condimentum diam rutrum. Duis pulvinar at turpis vitae dignissim.
+            Proin vehicula dignissim cursus.
+          </Typography>
+          <Button variant="contained">Learn more about our projects!</Button>
         </div>
       </Container>
     </div>
