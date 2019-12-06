cf65c214be2b3b73ed119d22e3ac5351769af094
ForkBombGIT <erosdipede@gmail.com>
2019-12-06 12:58:13 -0500

    Fix linting issues

--- tbw-website/src/Pages/LandingPage/LandingPage.jsx
+++ tbw-website/src/Pages/LandingPage/LandingPage.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import CustomDivider from "Components/CustomDivider/CustomDivider";
 import { Typography, Container, Button } from "@material-ui/core/";
 import landing from "Assets/images/landing.png";
-import services_background from "Assets/images/divider.png";
+import servicesBackground from "Assets/images/divider.png";
 
 import "./LandingPage.scss";
 
-const landing_section_services = {
-  backgroundImage: "url(" + { services_background } + ")"
+const landingSectionServices = {
+  backgroundImage: `url(${{ servicesBackground }})`
 };
 
 function LandingPage() {
   return (
-    <div class="landing">
-      <div class="landing-title">
-        <div class="landing-title-text">
+    <div className="landing">
+      <div className="landing-title">
+        <div className="landing-title-text">
           <Typography variant="h1">Thompson Boiler Works</Typography>
           <Typography variant="h3">Welding since 1988</Typography>
         </div>
       </div>
-      <CustomDivider></CustomDivider>
-      <div class="landing-section">
+      <CustomDivider />
+      <div className="landing-section">
         <Container>
           <Typography variant="h2" align="center">
             Who are we?
@@ -41,12 +41,12 @@ function LandingPage() {
           <Button variant="contained">Learn more about us!</Button>
         </Container>
       </div>
-      <div style={landing_section_services} class="landing-section">
+      <div style={landingSectionServices} className="landing-section">
         <Container>
           <Typography variant="h2" align="center">
             Services
           </Typography>
-          <div class="landing-services-body">
+          <div className="landing-services-body">
             <Typography className="landing-section-body" variant="h6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor urna quis
               sapien rutrum volutpat. Nullam lectus magna, lacinia nec orci quis, cursus convallis
@@ -56,8 +56,8 @@ function LandingPage() {
               dignissim cursus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
               porttitor urna quis sapien rutrum volutpat.
             </Typography>
-            <div class="landing-services-image">
-              <img src={landing} alt="services"></img>
+            <div className="landing-services-image">
+              <img src={landing} alt="services" />
               <Typography variant="h6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </Typography>
@@ -66,7 +66,7 @@ function LandingPage() {
           <Button variant="contained">Learn more about our services!</Button>
         </Container>
       </div>
-      <div class="landing-section">
+      <div className="landing-section">
         <Container>
           <Typography variant="h2" align="center">
             Projects
@@ -84,11 +84,11 @@ function LandingPage() {
             ut sem eleifend, quis condimentum diam rutrum. Duis pulvinar at turpis vitae dignissim.
             Proin vehicula dignissim cursus.
           </Typography>
-          <div class="landing-projects-image">
-            <img src={landing} alt="projects"></img>
-            <img src={landing} alt="projects"></img>
-            <img src={landing} alt="projects"></img>
-            <img src={landing} alt="projects"></img>
+          <div className="landing-projects-image">
+            <img src={landing} alt="projects" />
+            <img src={landing} alt="projects" />
+            <img src={landing} alt="projects" />
+            <img src={landing} alt="projects" />
           </div>
           <Button variant="contained">Learn more about our projects!</Button>
         </Container>
