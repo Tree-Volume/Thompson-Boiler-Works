97fa43c6ae86579ab6a77d9a3e046371ba354e5e
ForkBombGIT <erosdipede@gmail.com>
2019-12-10 22:57:08 -0500

    Add localization

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { CustomToolbar, CustomFooter, PageHeader } from "Components/";
 import { Container, Typography } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
 import aboutImage from "Assets/images/contact-page-header.jpg";
 import "./AboutPage.scss";
 
 const AboutPage = () => {
+  const { t } = useTranslation();
   return (
     <>
       <CustomToolbar />
-      <PageHeader imagePath={aboutImage} pageTitle="About" />
+      <PageHeader imagePath={aboutImage} pageTitle={t("about.title")} />
       <Container className="about-page">
         <div className="about-section">
           <Typography className="about-section-text" variant="h6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor urna quissapien
-            rutrum volutpat. Nullam lectus magna, lacinia nec orci quis, cursus convallismagna. Cras
-            dictum laoreet tellus a rutrum. Donec ac commodo augue. Donec enim metus,fringilla id
-            suscipit ac, malesuada non ligula. Aenean ultricies quam ut sem eleifend,quis
-            condimentum diam rutrum. Duis pulvinar at turpis vitae dignissim. Proin
-            vehiculadignissim cursus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Etiamporttitor urna quis sapien rutrum volutpat.
+            {t("about.section.one")}
           </Typography>
           <div className="about-section-image">
             <img src={aboutImage} alt="about 1" />
@@ -26,16 +22,10 @@ const AboutPage = () => {
         </div>
         <div className="about-section">
           <Typography className="about-section-text" variant="h6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor urna quissapien
-            rutrum volutpat. Nullam lectus magna, lacinia nec orci quis, cursus convallismagna. Cras
-            dictum laoreet tellus a rutrum. Donec ac commodo augue. Donec enim metus,fringilla id
-            suscipit ac, malesuada non ligula. Aenean ultricies quam ut sem eleifend,quis
-            condimentum diam rutrum. Duis pulvinar at turpis vitae dignissim. Proin
-            vehiculadignissim cursus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Etiamporttitor urna quis sapien rutrum volutpat.
+            {t("about.section.two")}
           </Typography>
           <div className="about-section-image">
-            <img src={aboutImage} alt="about 1" />
+            <img src={aboutImage} alt="about 2" />
           </div>
         </div>
       </Container>
