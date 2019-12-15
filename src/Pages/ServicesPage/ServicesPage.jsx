72fef4e42f0bc1a58943e44081f18d92a54b890e
ForkBombGIT <erosdipede@gmail.com>
2019-12-15 01:42:54 -0500

    [WIP] Add services page content

--- src/Pages/ServicesPage/ServicesPage.jsx
+++ src/Pages/ServicesPage/ServicesPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PageHeader } from "Components/";
-import { Container } from "@material-ui/core";
+import { Container, Typography } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import servicesImage from "Assets/images/contact-page-header.jpg";
 import "./ServicesPage.scss";
@@ -27,6 +27,28 @@ const ServicesPage = () => {
             |
             <Button> Services 7 </Button>
           </div>
+        </div>
+        <hr></hr>
+        <div className="flavor">
+          <Typography align="center" variant="h6" className="flavor-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
+        </div>
+        <div className="service">
+          <Container>
+            <Typography variant="h2">Service 1</Typography>
+            <Typography variant="h6" className="service-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor urna quissapien rutrum volutpat. Nullam lectus magna, lacinia nec orci quis, cursus convallismagna. Cras dictum laoreet tellus a rutrum. Donec ac commodo augue. Donec enim metus,fringilla id suscipit a</Typography>
+            <div className="service-image">
+              <img src={servicesImage} alt="service 2" />
+            </div>
+          </Container>
+        </div>
+        <div className="service">
+          <Container>
+            <Typography variant="h2">Service 2</Typography>
+            <Typography variant="h6" className="service-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor urna quissapien rutrum volutpat. Nullam lectus magna, lacinia nec orci quis, cursus convallismagna. Cras dictum laoreet tellus a rutrum. Donec ac commodo augue. Donec enim metus,fringilla id suscipit a</Typography>
+            <div className="service-image">
+              <img src={servicesImage} alt="service 2" />
+            </div>
+          </Container>
         </div>
       </Container>
     </>
