7d1109ac1a721c6aadb99ec5ba8f74546537d0c9
Liannus <josh-gorman@hotmail.com>
2019-12-29 17:00:04 -0500

    TBW-5 Added code for projects page

---
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { Typography, Container } from "@material-ui/core/";
+import { useTranslation } from "react-i18next";
+import { PageHeader, Project } from "Components/";
+import projectsImage from "Assets/images/contact-page-header.jpg";
+import "./ProjectsPage.scss";
+
+const ProjectsPage = props => {
+  const { t } = useTranslation();
+  useEffect(() => {
+    props.setNotLanding(true);
+  });
+
+  const projectsObject = t("projects.project", { returnObjects: true });
+
+  return (
+    <>
+      <PageHeader imagePath={projectsImage} pageTitle="Projects" />
+      <Container className="projects-page">
+        <div className="flavor">
+          <Typography align="center" variant="h6" className="flavor-text">
+            {t("services.flavorText")}
+          </Typography>
+        </div>
+        <div className="projects">
+          {projectsObject != null &&
+            Object.keys(projectsObject).map(key => (
+              <Project
+                key={key}
+                title={t(`projects.project.${key}.title`)}
+                body={t(`projects.project.${key}.body`)}
+                imageLink={t(`projects.project.${key}.imageLink`)}
+                imageAlt={t(`projects.project.${key}.imageAlt`)}
+              />
+            ))}
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default ProjectsPage;
