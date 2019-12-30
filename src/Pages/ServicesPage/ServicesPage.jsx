ea01f4a6c428a7d287fecf7ceffb6300bce00970
ForkBombGIT <erosdipede@gmail.com>
2019-12-30 15:44:31 -0500

    Add dynamic rendering based on localization files

--- src/Pages/ServicesPage/ServicesPage.jsx
+++ src/Pages/ServicesPage/ServicesPage.jsx
@@ -5,40 +5,51 @@ import { useTranslation } from "react-i18next";
 import servicesImage from "Assets/images/contact-page-header.jpg";
 import "./ServicesPage.scss";
 
-const ServicesPage = (props) => {
+const ServicesPage = props => {
   const { t } = useTranslation();
   useEffect(() => {
     props.setNotLanding(true);
   });
+  const servicesObject = t("services.service", { returnObjects: true });
+  function buildNavBar() {
+    const servicesKeys = Object.keys(servicesObject);
+    const nav = [];
+    do {
+      const keySplice = servicesKeys.splice(0, 4);
+      nav.push(
+        <div className="row">
+          {keySplice.map((key, index) => (
+            <>
+              <Button key={key}>{t(`services.service.${key}.title`)}</Button>
+              {index + 1 !== keySplice.length && <span>|</span>}
+            </>
+          ))}
+        </div>
+      );
+    } while (servicesKeys.length > 0);
+    return nav;
+  }
   return (
     <>
       <PageHeader imagePath={servicesImage} pageTitle={t("services.title")} />
       <Container className="services-page">
-        <div className="services-nav">
-          <div className="row">
-            <Button>{t("services.service.one.title")}</Button>
-            <span>|</span>
-            <Button>{t("services.service.two.title")}</Button>
-          </div>
-        </div>
+        <div className="services-nav">{buildNavBar()}</div>
         <hr className="linebreak" />
         <div className="flavor">
           <Typography align="center" variant="h6" className="flavor-text">
             {t("services.flavorText")}
           </Typography>
         </div>
-        <Service
-          title={t("services.service.one.title")}
-          content={t("services.service.one.body")}
-          image={servicesImage}
-          imageAlt={t("services.service.one.imageAlt")}
-        />
-        <Service
-          title={t("services.service.two.title")}
-          content={t("services.service.two.body")}
-          image={servicesImage}
-          imageAlt={t("services.service.two.imageAlt")}
-        />
+        {servicesObject != null &&
+          Object.keys(servicesObject).map(key => (
+            <Service
+              key={key}
+              title={t(`services.service.${key}.title`)}
+              content={t(`services.service.${key}.body`)}
+              image={servicesImage}
+              imageAlt={t(`services.service.${key}.imageAlt`)}
+            />
+          ))}
       </Container>
     </>
   );
