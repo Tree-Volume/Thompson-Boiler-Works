a5ef2578ddec3d9d382880e82a2015b1b5f417a1
Liannus <josh-gorman@hotmail.com>
2020-01-10 23:19:05 -0500

    TBW-41 Broke out unneeded code and tested translations

--- src/Components/CustomToolbar/CustomToolbar.jsx
+++ src/Components/CustomToolbar/CustomToolbar.jsx
@@ -2,51 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Menu, MenuItem, Toolbar, Button } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
-import routerPaths from "Utils/RouterPaths";
+import { returnFlagByLanguage, RouterPaths } from "Utils/";
 import "./CustomToolbar.scss";
 import logo from "Assets/images/tbw-logo.png";
-
-const returnFlag = lang => {
-  if (lang === "en") {
-    return (
-      <svg
-        className="flagIcon"
-        xmlns="http://www.w3.org/2000/svg"
-        id="flag-icon-css-gb"
-        viewBox="0 0 640 480"
-      >
-        <path fill="#012169" d="M0 0h640v480H0z" />
-        <path
-          fill="#FFF"
-          d="M75 0l244 181L562 0h78v62L400 241l240 178v61h-80L320 301 81 480H0v-60l239-178L0 64V0h75z"
-        />
-        <path
-          fill="#C8102E"
-          d="M424 281l216 159v40L369 281h55zm-184 20l6 35L54 480H0l240-179zM640 0v3L391 191l2-44L590 0h50zM0 0l239 176h-60L0 42V0z"
-        />
-        <path fill="#FFF" d="M241 0v480h160V0H241zM0 160v160h640V160H0z" />
-        <path fill="#C8102E" d="M0 193v96h640v-96H0zM273 0v480h96V0h-96z" />
-      </svg>
-    );
-  }
-  if (lang === "fr") {
-    return (
-      <svg
-        className="flagIcon"
-        xmlns="http://www.w3.org/2000/svg"
-        id="flag-icon-css-fr"
-        viewBox="0 0 640 480"
-      >
-        <g fillRule="evenodd" strokeWidth="1pt">
-          <path fill="#fff" d="M0 0h640v480H0z" />
-          <path fill="#00267f" d="M0 0h213.3v480H0z" />
-          <path fill="#f31830" d="M426.7 0H640v480H426.7z" />
-        </g>
-      </svg>
-    );
-  }
-  return null;
-};
 
 const CustomToolbar = props => {
   const [toolbarColor, setToolbarColor] = useState("#0b121000");
@@ -71,7 +29,8 @@ const CustomToolbar = props => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleClose = lang => {
+    i18n.changeLanguage(lang);
     setAnchorEl(null);
   };
 
@@ -92,7 +51,7 @@ const CustomToolbar = props => {
       style={{ backgroundColor: toolbarColor, transition: "background-color 0.2s" }}
     >
       <Toolbar>
-        <Link className="toolbar-logo" to={routerPaths.LANDING}>
+        <Link className="toolbar-logo" to={RouterPaths.LANDING}>
           <div className="toolbar-logo-image">
             <img src={logo} alt={title} />
           </div>
@@ -110,7 +69,7 @@ const CustomToolbar = props => {
           aria-haspopup="true"
           onClick={handleClick}
         >
-          {returnFlag(i18n.language)}
+          {returnFlagByLanguage(i18n.language)}
           {i18n.language}
         </Button>
         <Menu
@@ -120,12 +79,12 @@ const CustomToolbar = props => {
           open={Boolean(anchorEl)}
           onClose={handleClose}
         >
-          <MenuItem onClick={handleClose}>
-            {returnFlag("en")}
+          <MenuItem onClick={() => handleClose("en")}>
+            {returnFlagByLanguage("en")}
             en
           </MenuItem>
-          <MenuItem onClick={handleClose}>
-            {returnFlag("fr")}
+          <MenuItem onClick={() => handleClose("fr")}>
+            {returnFlagByLanguage("fr")}
             fr
           </MenuItem>
         </Menu>
