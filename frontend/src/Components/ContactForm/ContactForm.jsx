27637002ca35311462fa1b4f6834e3fca0b995e6
Eros Di Pede <eros.di.pede@ifsworld.com>
2020-01-24 16:09:23 -0500

    Add frontend validation to ContactForm

---
+++ frontend/src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Button, TextField } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
+import { useForm, Controller } from "react-hook-form";
+import sendEmail from "Utils/EmailSending";
+
+import "./ContactForm.scss";
+
+const defaultValues = {
+  Name: "",
+  Email: "",
+  Subject: "",
+  Body: ""
+};
+
+const ContactForm = () => {
+  const { handleSubmit, reset, control } = useForm({ defaultValues });
+  const { t } = useTranslation();
+
+  //if form passes validation, send email
+  const onSubmit = data => {
+    const emailParameters = {
+      origin: "CONTACT",
+      name: data.Name,
+      from: data.Email,
+      subject: data.Subject,
+      body: data.Body
+    };
+    sendEmail(emailParameters);
+    reset(defaultValues);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="contact-form">
+      <p className="contact-form-title">{t("contact.form.tagline")}</p>
+      <Controller
+        as={<TextField label={t("contact.form.name")} variant="filled" />}
+        name="Name"
+        control={control}
+        rules={{ required: true, maxLength: 80 }}
+      />
+      <Controller
+        as={<TextField label={t("contact.form.email")} variant="filled" />}
+        name="Email"
+        control={control}
+        rules={{ required: true, pattern: /^\S+@\S+$/i }}
+      />
+      <Controller
+        as={<TextField label={t("contact.form.subject")} variant="filled" />}
+        name="Subject"
+        control={control}
+        rules={{ required: true, maxLength: 40 }}
+      />
+      <Controller
+        as={<TextField label={t("contact.form.body")} multiline rows="5" variant="filled" />}
+        name="Body"
+        control={control}
+        rules={{ required: true, maxLength: 500 }}
+      />
+      <Button variant="contained" type="submit">
+        {t("contact.form.button")}
+      </Button>
+    </form>
+  );
+};
+
+export default ContactForm;
--- frontend/src/Pages/ContactPage/ContactPage.jsx
+++ frontend/src/Pages/ContactPage/ContactPage.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { PageHeader } from "Components/";
-import { Button, Container, TextField } from "@material-ui/core";
-import sendEmail from "Utils/EmailSending";
+import { PageHeader, ContactForm } from "Components/";
+import { Container } from "@material-ui/core";
 import contactImage from "Assets/images/contact-page-header.jpg";
 import "./ContactPage.scss";
 
 const ContactPage = props => {
   const { t } = useTranslation();
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [subject, setSubject] = useState();
-  const [body, setBody] = useState();
   useEffect(() => {
     props.setNotLanding(true);
   });
@@ -19,46 +14,7 @@ const ContactPage = props => {
     <>
       <PageHeader imagePath={contactImage} pageTitle="Contact" />
       <Container className="contact-page">
-        <div className="contact-form">
-          <p className="contact-form-title">{t("contact.form.tagline")}</p>
-          <TextField
-            label={t("contact.form.name")}
-            variant="filled"
-            onChange={e => setName(e.target.value)}
-          />
-          <TextField
-            label={t("contact.form.email")}
-            variant="filled"
-            onChange={e => setEmail(e.target.value)}
-          />
-          <TextField
-            label={t("contact.form.subject")}
-            variant="filled"
-            onChange={e => setSubject(e.target.value)}
-          />
-          <TextField
-            label={t("contact.form.body")}
-            multiline
-            rows="5"
-            variant="filled"
-            onChange={e => setBody(e.target.value)}
-          />
-          <Button
-            variant="contained"
-            onClick={() => {
-              const emailParameters = {
-                origin: "CONTACT",
-                name: name,
-                from: email,
-                subject: subject,
-                body: body
-              };
-              sendEmail(emailParameters);
-            }}
-          >
-            {t("contact.form.button")}
-          </Button>
-        </div>
+        <ContactForm />
         <div className="contact-info">
           <iframe
             title="map"
