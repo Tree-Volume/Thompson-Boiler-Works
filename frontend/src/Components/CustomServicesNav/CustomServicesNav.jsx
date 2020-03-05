65a042fe56c2f63585cc82bec78a7bf88549c8b4
erdica <erdica@ifsworld.com>
2020-03-05 13:16:13 -0500

    Add ScrollSpy to CustomServicesNav

--- frontend/src/Components/CustomServicesNav/CustomServicesNav.jsx
+++ frontend/src/Components/CustomServicesNav/CustomServicesNav.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { Link, Typography } from "@material-ui/core/";
+import Scrollspy from "react-scrollspy";
 import MediaQuery from "react-responsive";
 import "./CustomServicesNav.scss";
 
@@ -15,6 +16,14 @@ const CustomServicesNav = props => {
       behavior: "smooth"
     });
   };
+  const navIds = () => {
+    const servicesKeys = Object.keys(props.servicesObject);
+    var ids = [];
+    for (var i = 0; i < servicesKeys.length; i++) {
+      ids.push(`service-${i}`);
+    }
+    return ids;
+  };
   //builds the navigation
   const buildNav = () => {
     const servicesKeys = Object.keys(props.servicesObject);
@@ -23,11 +32,13 @@ const CustomServicesNav = props => {
       const keySplice = servicesKeys.splice(0, 4);
       nav.push(
         <div className="row">
-          {keySplice.map((key, index) => (
-            <Link component="button" key={key} onClick={() => handleClick(index)}>
-              <Typography variant="h3">{t(`services.service.${key}.title`)}</Typography>
-            </Link>
-          ))}
+          <Scrollspy items={navIds()} componentTag="div" currentClassName="active" offset={ -100 }>
+            {keySplice.map((key, index) => (
+              <Link component="button" key={key} onClick={() => handleClick(index)}>
+                <Typography variant="h3">{t(`services.service.${key}.title`)}</Typography>
+              </Link>
+            ))}
+          </Scrollspy>
         </div>
       );
     } while (servicesKeys.length > 0);
