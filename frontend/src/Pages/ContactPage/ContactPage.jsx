282c6d2dadaea323c89954910085cd062d4fe1e7
Liannus <josh-gorman@hotmail.com>
2020-03-04 22:23:27 -0500

    TBW-59 implemented S3 secret bucket and local secrets

--- frontend/src/Pages/ContactPage/ContactPage.jsx
+++ frontend/src/Pages/ContactPage/ContactPage.jsx
@@ -2,6 +2,7 @@ import React, { useLayoutEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { PageHeader, ContactForm } from "Components/";
 import { Container } from "@material-ui/core";
+import secrets from "secret/secret.json";
 import contactImage from "Assets/images/contact-page-header.jpg";
 import "./ContactPage.scss";
 
@@ -22,7 +23,7 @@ const ContactPage = props => {
             height="50%"
             frameBorder="0"
             style={{ border: 0 }}
-            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJZy1pVWMKzkwRzSNkeLZIx5s&key="
+            src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJZy1pVWMKzkwRzSNkeLZIx5s&key=${secrets.GMAPS_API_KEY}`}
             allowFullScreen
           />
           <div className="contact-info-div">
