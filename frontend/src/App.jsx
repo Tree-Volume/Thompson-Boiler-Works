0e45c65de11380f5b93a8873851bcde6b9947f21
ForkBombGIT <erosdipede@gmail.com>
2020-07-28 20:14:41 -0400

    Add router for internet explorer users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,31 +15,54 @@ import "./Styles/App.scss";
 const App = () => {
   // to-do, setNotLanding to true when not on landing page
   const [notLanding, setNotLanding] = useState(false);
+  const usingIe = () => {
+    // Internet Explorer 6-11
+    const isIE = /*@cc_on!@*/ false || !!document.documentMode;
+    return isIE;
+  };
+  const buildRoutes = () => {
+    const landingRoute = (
+      <Route path={RouterPaths.LANDING}>
+        <LandingPage setNotLanding={setNotLanding} />
+      </Route>
+    );
+    // Return IE router (only landing)
+    if (usingIe()) {
+      return (
+        <Switch>
+          {landingRoute}
+        </Switch>
+      )
+    }
+    // Return normal router
+    return (
+      <Switch>
+        <Route path={RouterPaths.ABOUT}>
+          <AboutPage setNotLanding={setNotLanding} />
+        </Route>
+        <Route path={RouterPaths.SERVICES}>
+          <ServicesPage setNotLanding={setNotLanding} />
+        </Route>
+        <Route path={RouterPaths.CAREERS}>
+          <CareersPage setNotLanding={setNotLanding} />
+        </Route>
+        <Route path={RouterPaths.CONTACT}>
+          <ContactPage setNotLanding={setNotLanding} />
+        </Route>
+        <Route path={RouterPaths.PROJECTS}>
+          <ProjectsPage setNotLanding={setNotLanding} />
+        </Route>
+        {landingRoute}
+      </Switch>
+    );
+  }
+
   return (
     <Router>
       <ScrollToTop />
       <div className="App">
         <CustomToolbar notLanding={notLanding} />
-        <Switch>
-          <Route path={RouterPaths.ABOUT}>
-            <AboutPage setNotLanding={setNotLanding} />
-          </Route>
-          <Route path={RouterPaths.SERVICES}>
-            <ServicesPage setNotLanding={setNotLanding} />
-          </Route>
-          <Route path={RouterPaths.CAREERS}>
-            <CareersPage setNotLanding={setNotLanding} />
-          </Route>
-          <Route path={RouterPaths.CONTACT}>
-            <ContactPage setNotLanding={setNotLanding} />
-          </Route>
-          <Route path={RouterPaths.PROJECTS}>
-            <ProjectsPage setNotLanding={setNotLanding} />
-          </Route>
-          <Route path={RouterPaths.LANDING}>
-            <LandingPage setNotLanding={setNotLanding} />
-          </Route>
-        </Switch>
+        {buildRoutes()}
         <CustomFooter />
       </div>
     </Router>
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,9 +2,10 @@ import 'react-app-polyfill/ie11';
 import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"
 import App from "./App";
 import "i18n";
 import "Styles/Fonts.scss";
 import "Styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
